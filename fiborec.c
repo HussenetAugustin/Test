4a596e9460e4f367b0a3c0949225cf46685fbdec
Augustin Hussenet <hussenet7u@a-mn-p322-pc17.artem.site.univ-lorraine.fr>
2019-06-21 18:39:51 +0200

    Validé

---
+++ fiborec.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int f (int n){
+		if (n==0){
+			return 0;
+		} else if (n==1) {
+			return 1;
+		}
+		return f(n-1)+f(n-2);
+	}
+
+	int x=f(15);
+	printf("%d\n",x)
+}
